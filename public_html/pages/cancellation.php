1bee8800e0fa4ec6dc367d9b20907dd6f114aa00
Syed Anees Ibrahim <syedaneesibrahom@gmail.com>
2024-05-09 11:56:43 +0530

    content changes

--- public_html/pages/cancellation.php
+++ public_html/pages/cancellation.php
@@ -13,42 +13,33 @@
     <article>
       
 
-      <div class="container pb-5 pt-5">
+      <div class="container terms-container pb-5 pt-5">
           <h1 class="main-heading">Cancellation Policy</h1>
-         <p>Travel Fares truly believes in transparency and fair dealing, therefore, during hassle-free flight reservations, we provide an invoice enlisting the low fares, reasonable taxes, shopping fee and cost of additional services. This helps you in knowing what all are airlines paying for without any confusion, in case of flight cancellation or refund.</p>
+          <ul>
+  <li>By making an installment or payment towards your tour package or other services through travel-fares.com, you agree to allow us to continue processing and delivering your required services.</li>
+  <li>When reserving a spot with us, you acknowledge and agree to the Trip Tour Provider's cancellation and no-show policy, as well as any additional terms and conditions of the Trip Tour Provider applicable to your trip (including fine print and house rules), for services or products offered by the Trip Tour Provider.</li>
+  <li>Hotel reservations are subject to availability, and room rates may change without prior notice.</li>
+  <li>In the event that the originally booked lodging is unavailable, Travelio Tourism And Travel Online reserves the right to secure a booking in a similar category based on availability.</li>
+  <li>travel-fares.com reserves the right to cancel payments or orders before delivery without stating reasons.</li>
+  <li>You may cancel an order within 7 working days of email confirmation from travel-fares.com. However, with your express consent, we will begin delivery immediately.</li>
+  <li>Failure to arrive at the venue for a scheduled activity will result in the cancellation of your booking, and we reserve the right to deny any refund requests in such cases.</li>
+  <li>Order cancellations will incur a pre-order processing charge of 50 US Dollars per booking.</li>
+  <li>Once your booking is approved by Travelio Tourism And Travel, no refunds can be provided, and you must adhere to the written policy.</li>
+  <li>Bookings made or confirmed at the United Arab Emirates immigration desk cannot be canceled.</li>
+  <li>The company will not be liable for natural disasters, loss of goods, or any related events during the tour.</li>
+  <li>Customers must provide return air tickets and visas to travel-fares.com before booking a tour package for a specific country. Failure to do so will result in the booking not being confirmed. If a customer pays for a tour package without prior air ticket and visa bookings, or if they are unable to provide them after payment, the booking must be canceled, and a 20% cancellation fee will apply. The remaining amount will be refunded to the customer's source of payment.</li>
+  <li>Additional service insurance is provided by a third party, and in case of a refund, a 25% deduction will be made for pre-processing and payment gateway charges.</li>
+  <li>The service will begin at the specified location, disclosed upon confirmation of the tour package reservation. If airport transfer is included, our driver will be waiting at the designated airport area with a name sign. The customer must approach the driver to avail the service. Failure to do so will result in being marked as a "No Show," and no refund will be issued.</li>
+  <li>Tour package costs listed on travel-fares.com are subject to change, especially during peak seasons. Our staff will notify you of any changes, and you may need to make an additional payment or adjust your travel dates. Ensuring your booking matches the amount you paid is our priority.</li>
+  <li>If there are changes to the travel date, time, flight, or flight number after service confirmation, the customer must notify Travel-fares.com. Failure to do so may result in being marked as a "No Show," and no refund will be issued.</li>
+  <li>Tour package bookings can be canceled if the cancellation request is made before the service is reserved and confirmed by Travel-fares.com, and the trip date is more than three days away. A 25% cancellation fee will be withheld, with the remaining amount returned within three to four working days.</li>
+  <li>No refund will be given for cancellations made less than 72 hours before the scheduled service time.</li>
+  <li>Changes to the service can be made up to 72 hours before the scheduled service time, but additional fees may apply. Changes made less than 72 hours before the scheduled service time will result in a "No Show," and no amendments will be permitted.</li>
+  <li>Any denied service, including no-shows, will be charged in full, and no refunds will be issued.</li>
+  <li>In case of lost goods or luggage, the client and the driver should communicate to retrieve the goods/luggage. Travel-fares.com disclaims all liability for losses.</li>
+  <li>Customers should contact our team for any changes or additional requests to their reservation.</li>
+</ul>
 
-         <p><b>Below are some of the things that Travel Fares wants to know while making trouble-free flight cancellation and refunds:</b></p>
-
-         <ul class="list-names">
-             <li class="inner-list">Customers have an authority to get full refunds only if your flight ticket is refundable. Therefore, Travel Fares advises you to go through all the terms and conditions of respective airlines before making flight reservations.</li>
-             <li class="inner-list">Additional charges like taxes and other fee are influenced by government and will either be included in your flight fare or will be itemized on checkout pages to tell you the actual cost of your journey.</li>
-             <li class="inner-list">If your ticket is refundable, you'll be charged an extra fee by Travel Fares customer support for all the flight ticket cancellations.</li>
-             <li class="inner-list">Allows you to cancel your existing flight bookings without paying any additional charges if cancellation is done within 24 hours of original purchases. Customers may contact the dedicated team in case of any query or issue related to reservations, cancellations, refunds, etc.</li>
-             <li class="inner-list">A refund request will be accepted if the cancellation and refunds have been applied through Travel Fares and the particular airline allows the same.</li>
-             <li class="inner-list">Customer who didn't reach before the scheduled flight time, will not be eligible for any kind of refunds.</li>
-             <li class="inner-list"> Airlines have full authority to charge a penalty on the refunded fares in case of any cancellation.</li>
-             <li class="inner-list">Travel Fares is not responsible for any fluctuation in taxes while making flight reservations. A non-refundable fee that will be charged for every ticket has been mentioned as an actual ticket price.</li>
-            <li class="inner-list">If your journey needs an issuance of the paper tickets, then only the handling and shipping fees will be charged.</li>
-            </ul>
-
-
-            <h3>Cancellation and Refunds</h3>
-            <p>The air travel journey is supposed to change if it is permitted by the airline fare rules and you might need to pay the flight change fee which is charged by the airline itself, ranging from $200 and $500 on every flight ticket. The rate may fluctuate depending on the carrier, market and specific fare rules.</p>
-
-            <p>Travel Fares does not include Low-Cost Carriers (LCCs) under its cancellation and refund policy. It means all the flight tickets booked for an LCC are non-refundable even if you cancel the ticket within 24 hours of original purchases.</p>
-
-
-
-
-
-
-
-
-
-
-
-
-        </div>
     </article>
   </section>
 
--- public_html/pages/privacy.php
+++ public_html/pages/privacy.php
@@ -15,141 +15,66 @@
 
       <div class="container pb-5 pt-5">
           <h2 class="main-heading">Privacy Policy</h2>
-          <p>Travel Fares believes in providing full satisfaction to the customers and tends in maintaining good relations with everyone. Our top priority is to keep all your personal details safe and confidential. Your safety and satisfaction is our main duty. The privacy policy will provide you with clear information about our policies. Below are the details of the personal data that is collected by us:</p>
-            <p>We get and store the personal details that you enter while making flight reservations in various ways. These details include your first and last name, phone number, address, email ID, card details, etc. Customers may choose not to provide a few of all such information but for that must contact our dedicated team of professionals. The experts are available 24*7 to assist you and guide you with the best.</p>
-        <p>When you make flight reservations for another customer through our site, you will be asked for all the concerned customer's personal details. Therefore, it is advised to must concern the person before providing their personal data.</p>
-        <p>We may require individual and non-individual details about the customer for various reasons like business accomplices, to maintain our record details. We may require other details like updated delivery and address information, reservation history and statistic data.</p>
-        <p>Whenever a customer visits our site, we collect some information about your computers like your IP address, insights about the browser, etc. The purpose of collecting all the data incorporates your travelling experience and for improving our services.</p>
+            <p>The Privacy Policy of Travel-fares.com is designed to inform website users about the
+            protection and use of their personal data. This site is dedicated to protecting the personal
+            information provided by users for the purpose of booking tour packages and other travel
+            services. The privacy policies described here will be updated regularly, and we recommend
+            checking this page often for any changes. By using our services, you agree to comply with
+            this privacy policy and its terms and conditions. Please be aware that Travel-fares.com
+            reserves the right to amend and/or modify this statement at any time.</p>
 
-        <h3>How we use collected data?</h3>
-        <p>While keeping all your data safe and secured, we use some details like billing details including cardholder name, card number and expiration date to execute the travel reservations that you made through our site. We also use details for the following reasons:</p>
-        <ul class="list-names">
-            <li class="inner-list"><svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="#333" viewbox="0 0 512 512"><title>Chevron Forward</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M184 112l144 144-144 144"></path></svg> To deal with customer's record that helps in preparing bills and providing notifications related to budget-friendly travelling.</li>
-            <li class="inner-list"><svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="#333" viewbox="0 0 512 512"><title>Chevron Forward</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M184 112l144 144-144 144"></path></svg> To enhance our services, administration and site</li>
-            <li class="inner-list"><svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="#333" viewbox="0 0 512 512"><title>Chevron Forward</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M184 112l144 144-144 144"></path></svg> To provide you with travelling conformation</li>
-            <li class="inner-list"><svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="#333" viewbox="0 0 512 512"><title>Chevron Forward</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M184 112l144 144-144 144"></path></svg> To resolve your queries and issues</li>
-            <li class="inner-list"><svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="#333" viewbox="0 0 512 512"><title>Chevron Forward</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M184 112l144 144-144 144"></path></svg> To make you aware of programs that you may join</li>
-            <li class="inner-list"><svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" fill="#333" viewbox="0 0 512 512"><title>Chevron Forward</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="48" d="M184 112l144 144-144 144"></path></svg> To avoid illegal practices</li>
-        </ul>
+        <h3>Collected Information</h3>
+        <p>
+        Your information is handled with the utmost confidentiality and is never shared with third
+        parties, unless required by law or expressly permitted by you for specific purposes. We
+        prioritize protecting your information from unauthorized use, except when necessary to
+        comply with legal obligations or for our own safety. Rest assured that appropriate measures
+        are taken to ensure the security of your information.
+        </p>
 
-        <h4>With whom we share your details?</h4>
-        <p>Independent sellers who provide services on our behalf include client benefit, business examination, survey distribution and marketing, fraud prevention. We also offer authorization to independent merchants for collecting some information on our behalf as it is important for improving the web-based advertising that will be customized according to customer's interests.</p>
+        
+        <h3>Cookies</h3>
+        <p>
+        A cookie is a very small file stored on your computer with your permission, designed to
+improve your browsing experience and help us understand your preferences better. For
+instance, a traffic log cookie helps us collect information about how you use our website,
+like which pages you visit. This data allows us to analyze and enhance our website&#39;s
+performance and services. While most browsers are set to accept cookies by default, you
+have the option to accept or decline them. If your browser is set to default settings, you can
+change them to manage your cookie preferences.
+        </p>
+        <h3>Disclaimer</h3>
+        <p>
+        Travel-fares.com recognizes that there are inherent security risks in communicating and
+providing services over the Internet. The company will not be held responsible for any
+damages or losses, whether direct or indirect, incidental or consequential, special or
+punitive, arising from unauthorized access, alteration, deletion, or exposure of any
+information, whether confidential or not. This includes unauthorized intrusion or security
+breaches, as well as system failures. Travel-fares.com does not offer warranties regarding
+the hardware and software used to provide security and support for its website, including
+any implied warranties of fitness for a particular purpose. Travel-fares.com, its officers,
 
+employees, partners, affiliates, subsidiaries, successors, assigns, and third-party agents will
+not be liable, directly or indirectly, for any inaccuracies, misuse, errors, third-party
+interceptions, viruses, or hacker attacks resulting in data loss or service disruptions. This
+includes errors or interruptions in service transmission or delivery. Travel-fares.com
+provides links to other websites for informational purposes and to facilitate access to
+additional online resources, but it is not responsible for the privacy practices or content of
+these external websites.
+        </p>
 
-        <h5>Safety and security of your data</h5>
-        <p>While availing our services, you may find links to the third party and other services. Must note that if you click on these links then you'll be redirected to some other site, but as per our policies we won't disclose any of your details to them. While collecting your details we ensure to enhance our database to improve our functionality and services and keep in mind your safety. Therefore, we do not involve any third party to use your database.</p>
-
+        <h3>Summary</h3>
+        <p>
+        By using this website, you explicitly agree to the terms set forth in this Privacy Statement
+and our terms and conditions. You agree to comply with the terms that govern the use of
+this site and its services, including any information you or other users provide to Travel-
+fares.com. If you do not fully agree with all the terms outlined in this Privacy Statement,
+please refrain from using this website.
+        </p>
+      
+      
         </div>
     </article>
   </section>
 
-        <div class="container pt-5">
-            <div class="row">
-                <div class="col-lg-3 col-md-3 col-sm-12 mb-4">
-                    
-                    <a href="tel:+1-888-814-6264" class="foot-call"><svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill="#fff" viewbox="0 0 512 512"><title>+1-888-814-6264</title><path d="M391 480c-19.52 0-46.94-7.06-88-30-49.93-28-88.55-53.85-138.21-103.38C116.91 298.77 93.61 267.79 61 208.45c-36.84-67-30.56-102.12-23.54-117.13C45.82 73.38 58.16 62.65 74.11 52a176.3 176.3 0 0128.64-15.2c1-.43 1.93-.84 2.76-1.21 4.95-2.23 12.45-5.6 21.95-2 6.34 2.38 12 7.25 20.86 16 18.17 17.92 43 57.83 52.16 77.43 6.15 13.21 10.22 21.93 10.23 31.71 0 11.45-5.76 20.28-12.75 29.81-1.31 1.79-2.61 3.5-3.87 5.16-7.61 10-9.28 12.89-8.18 18.05 2.23 10.37 18.86 41.24 46.19 68.51s57.31 42.85 67.72 45.07c5.38 1.15 8.33-.59 18.65-8.47 1.48-1.13 3-2.3 4.59-3.47 10.66-7.93 19.08-13.54 30.26-13.54h.06c9.73 0 18.06 4.22 31.86 11.18 18 9.08 59.11 33.59 77.14 51.78 8.77 8.84 13.66 14.48 16.05 20.81 3.6 9.53.21 17-2 22-.37.83-.78 1.74-1.21 2.75a176.49 176.49 0 01-15.29 28.58c-10.63 15.9-21.4 28.21-39.38 36.58A67.42 67.42 0 01391 480z"></path></svg> +1-888-814-6264</a>
-                    <div><br><br><br><h3>CONTACT ADDRESS:</h3><p>Travelio Tourism And Travel<br>Address- AL SAFIYA - BLDG, Hor Al Anz, Deira, Dubai(UAE)<p></div>
-                </div>
-
-                <div class="col-lg-2 col-md-2 col-sm-12 mb-4">
-                    <h3 class="h6">Quick Links</h3>
-                    <ul>
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="info.html">Travel Info</a></li>
-                        <li><a href="about-us.html">About us</a></li>
-                        <li><a href="contact-us.html">Contact Us</a></li>
-                        <li><a href="privacy.html">Privacy Policy</a></li>
-                        <li><a href="cancellation.html">Cancellation Policy</a></li>
-                        <li><a href="terms.html">Terms &amp; Conditions</a></li>
-                        <!--<li><a href="sitemap.php">Sitemap</a></li>-->
-                    </ul>
-
-                </div>
-                <div class="col-lg-2 col-md-2 col-sm-12 mb-4">
-                    <h3 class="h6">Top Airlines</h3>
-                    <ul>
-                        <li><a href="alaska-airlines.html">Alaska Airlines</a></li>
-                        <li><a href="delta-airlines.html">Delta Airlines</a></li>
-                        <li><a href="lufthansa-airlines.html">Lufthansa</a></li>
-                        <li><a href="united-airlines.html">United Airlines</a></li>
-                        <li><a href="american-airlines.html">American Airlines</a></li>
-                        <li><a href="etihad-airlines.html">Etihad Airways</a></li>
-                        <li><a href="qatar-airways.html">Qatar Airways</a></li>
-                        <li><a href="southwest-airlines.html">Southwest Airlines</a></li>
-                        <li><a href="jetblue-airlines.html">Jetblue Airlines</a></li>
-                        <li><a href="hawaiian-airlines.html">Hawaiian Airlines</a></li>
-                    </ul>
-
-                </div>
-                
-                <div class="col-lg-2 col-md-2 col-sm-12 mb-4">
-                    <h3 class="h6">Top Airlines</h3>
-                    <ul>
-                        <li><a href="virgin-america-airlines.html">Virgin Atlantic</a></li>
-                        <li><a href="#">Brussels Airlines</a></li>
-                        <li><a href="top-airlines.html">TAP AIR</a></li>
-                        <li><a href="#">KLM Airlines</a></li>
-                        <li><a href="#">Singapore Airlines</a></li>
-                        <li><a href="british-airways.html">British Airways</a></li>
-                        <li><a href="#">Emirates</a></li>
-                        <li><a href="#">Turkish Airlines</a></li>
-                        <li><a href="#">Air France</a></li>
-                        <li><a href="copa-airlines.html">Cathay Pacific Airlines</a></li>
-                    </ul>
-
-                </div>
-
-                <div class="col-lg-3 col-md-3 col-sm-12 mb-4">
-                    <h3 class="h6">Top Destinations</h3>
-                    <ul>
-                        <li><a href="flights-to-las-vegas.html">Flights to Las Vegas</a></li>
-                        <li><a href="flights-to-new-york.html">Flights to New York</a></li>
-                        <li><a href="flights-to-san-francisco.html">Flights to San Francisco</a></li>
-                        <li><a href="flights-to-orlando.html">Flights to Orlando</a></li>
-                        <li><a href="flights-to-boston.html">Flights to Boston</a></li>
-                        <li><a href="flights-to-paris.html">Flights to Paris</a></li>
-                       <!-- <li><a href="flights-to-rome.php">Flights to Rome</a></li>
-                        <li><a href="flights-to-macau.php">Flights to Macau</a></li>
-                        <li><a href="flights-to-atlanta.php">Flights to Atlanta</a></li>-->
-                    </ul>
-
-                </div>
-
-                
-
-
-
-
-
-
-
-
-
-
-            </div>
-        </div>
-
-        <div class="container br-top pt-4 pb-4">
-            <h4>Disclaimer</h4>
-            <p>In case of any requirement of information or you have any query related to our site’s disclaimer, please do reach out to us via email at (support@Travel-fares.com) or call at 1-888-814-6264. All the mentioned information on our website is published for general information purposes through utmost good faith. Said that, Travelio Tourism And Travel does not make any warranties about the completeness, accuracy as well as the reliability of this information. Moreover, Travelio Tourism And Travel will not stand responsible in case e of any damage or loss in connection with the usage of our website. Please ensure that you check the privacy policy and the terms of service as you come over or leave our website. Besides, we are very clear with our serving purpose and disclaimer, so please go through all the necessary information before engaging in any kind of business or uploading any private information. Also, the site owner has the authority to change the outdated links or links that have turned ‘bad’ in any period of time in the future.</p>
-
-            
-
-            
-
-        </div>
-
-        <div class="br-top pb-4">
-            <div class="container">
-                <div class="row">
-                    
-
-                    <div class="col-lg-6 col-md-5 col-sm-12 pt-4 text-end">
-                        <p>&copy; Copyright 2024 Travelio Tourism And Travel. All Rights Reserved.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-
+     
